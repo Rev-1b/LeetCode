28f52863498239ef0142bb34593b00d823d56cc5
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-09-26 16:17:46 +0400

    икгр2

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
-class Solution:
-    def checkStraightLine(self, coordinates: list[list[int]]) -> bool:
-        try:
-            equation = self.give_equation(coordinates[0], coordinates[1])
-        except ValueError:
-            return False
-        for point in coordinates[2:]:
-            if not equation(point):
-                return False
-        return True
+def data_merge(dataset: list[list[str]]) -> list[tuple[int, str]]:
+    result = []
+    for cluster in dataset:
+        if not result:
+            for node in cluster:
+                date1, info1 = node.split(' ', 1)
+                result.append((int(date1), info1))
 
-    @staticmethod
-    def give_equation(a: tuple, b: tuple):
-        x0, y0 = a
-        x1, y1 = b
-        if x0 == x1 or y0 == y1:
-            raise ValueError
-        return lambda point: (point[0] - x0) / (x1 - x0) == (point[1] - y0) / (y1 - y0)
+        else:
+            for node in cluster:
+                date, info = node.split(' ', 1)
+                date = int(date)
+                if date <= result[0][0]:
+                    result.insert(0, (date, info))
+
+                if date >= result[-1][0]:
+                    result.append((date, info))
+
+                start, end = 0, len(result) - 1
+                while start <= end:
+                    mid = (start + end) // 2
+                    if result[mid - 1][0] < date <= result[mid][0]:
+                        result.insert(mid, (date, info))
+                        break
+                    elif result[mid][0] > date and result[mid - 1][0] > date:
+                        end = mid - 1
+                    else:
+                        start = mid + 1
+    return result
 
 
-coordinates = [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]
+dataset = [
+    [
+        '1 amogus super',
+        '7 amogus super',
+        '13 amogus super',
+        '27 amogus super',
+        '29 amogus super',
+        '54 amogus super',
+        '96 amogus super'
+    ],
+    [
+        '2 amogus super',
+        '5 amogus super',
+        '10 amogus super',
+        '23 amogus super',
+        '34 amogus super',
+        '51 amogus super',
+        '87 amogus super'
+    ]
+]
 
 if __name__ == '__main__':
-    task = Solution()
-    print(task.checkStraightLine(coordinates))
+    for elem in data_merge(dataset):
+        print(elem)

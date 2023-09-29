39b4aeb70296095a90af724bc717f1e283ce2fef
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-09-29 13:55:02 +0400

    Саня, где сотка))

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
-def data_merge(dataset: list[list[str]]) -> list[tuple[int, str]]:
-    result = []
-    for cluster in dataset:
-        if not result:
-            for node in cluster:
-                date1, info1 = node.split(' ', 1)
-                result.append((int(date1), info1))
+def sanya_task():
+    tmp = input('Введите последовательность целых чисел через пробел: ').split()
+    if ',' in tmp:
+        return 'Введите ЦЕЛЫЕ числа ЧЕРЕЗ ПРОБЕЛ'
+    seq = list(map(int, input().split()))
+    if len(tmp) < 2:
+        return "Последовательность должна состоять как минимуи из 2х элементов"
 
-        else:
-            for node in cluster:
-                date, info = node.split(' ', 1)
-                date = int(date)
-                if date <= result[0][0]:
-                    result.insert(0, (date, info))
+    longest_seq, l_seq_coords = 0, []  # Определение переменных
+    curr_seq, c_seq_coords = 0, []
+    status = None
 
-                if date >= result[-1][0]:
-                    result.append((date, info))
+    for i in range(1, len(seq)):
+        if seq[i] <= seq[i - 1]:  # Случай отрицательного роста
+            if status != 'dcr':  # Случай, когда последовательность только стала отрицательной
+                status = 'dcr'
+                curr_seq = 2
+                c_seq_coords = [i - 1, i]
+            else:                 # Последовательность уже была отрицательной
+                curr_seq += 1
+                c_seq_coords[1] = i
 
-                start, end = 0, len(result) - 1
-                while start <= end:
-                    mid = (start + end) // 2
-                    if result[mid - 1][0] < date <= result[mid][0]:
-                        result.insert(mid, (date, info))
-                        break
-                    elif result[mid][0] > date and result[mid - 1][0] > date:
-                        end = mid - 1
-                    else:
-                        start = mid + 1
-    return result
+        elif seq[i] > seq[i - 1]:  # Случай положительного роста
+            if status != 'inc':  # Случай, когда последовательность только стала положительной
+                status = 'inc'
+                curr_seq = 2
+                c_seq_coords = [i - 1, i]
+            else:                 # Последовательность уже была положительной
+                curr_seq += 1
+                c_seq_coords[1] = i
 
+        if curr_seq > longest_seq:
+            longest_seq = curr_seq
+            l_seq_coords = c_seq_coords
 
-dataset = [
-    [
-        '1 amogus super',
-        '7 amogus super',
-        '13 amogus super',
-        '27 amogus super',
-        '29 amogus super',
-        '54 amogus super',
-        '96 amogus super'
-    ],
-    [
-        '2 amogus super',
-        '5 amogus super',
-        '10 amogus super',
-        '23 amogus super',
-        '34 amogus super',
-        '51 amogus super',
-        '87 amogus super'
-    ]
-]
+    ans = (f'Самая длинная монотонно изменяющаяся последовательность представляет собой срез: {l_seq_coords}\n'
+           f'В представленной последовательности ее можно выделить, как: {", ".join(map(str, seq[:l_seq_coords[0]]))}'
+           f' || {", ".join(map(str, seq[l_seq_coords[0]:l_seq_coords[1] + 1]))} || '
+           f'{", ".join(map(str, seq[l_seq_coords[1] + 1:]))}')
+
+    return ans
+
 
 if __name__ == '__main__':
-    for elem in data_merge(dataset):
-        print(elem)
+    print(sanya_task())

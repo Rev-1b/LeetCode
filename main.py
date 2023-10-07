bcdda547851f2ee61a31bee0b3d078cc0e01ca2d
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-07 12:46:17 +0400

    Medium: Two Sum II - Input Array Is Sorted

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
-        i, j = 0, 0
-        while i < len(t):
-            if t[i] == s[j]:
-                j += 1
-                if j == len(s):
-                    return True
-            i += 1
-        return False
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            temp_sum = numbers[i] + numbers[j]
+            if temp_sum == target:
+                return [i + 1, j + 1]
+            if temp_sum > target:
+                j -= 1
+            else:
+                i += 1
 
 
-s = "abc"
-t = "ahbgdc"
+numbers = [2, 7, 11, 15]
+target = 9
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.isSubsequence(s, t))
+    print(task.twoSum(numbers, target))

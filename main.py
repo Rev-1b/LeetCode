814b52c4f56c46c2b6296c3be817099ebcf8fbe8
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-09 13:52:56 +0400

    Medium: Longest Substring Without Repeating Characters
    My solution depends on set() collection, but there is 5-7 % more optimal solution using dict().

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 class Solution:
-    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
-        result = 10 ** 7
-        prev_pointer = 0
-        curr_sum = 0
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        result = 1
+        last = 0
+        our_set = set(s[0])
 
-        for i in range(len(nums)):
-            curr_sum += nums[i]
-            while curr_sum >= target:
-                curr_sum -= nums[prev_pointer]
-                result = min(result, i + 1 - prev_pointer)
-                prev_pointer += 1
+        for i in range(1, len(s)):
+            while s[i] in our_set:
+                our_set.remove(s[last])
+                last += 1
+            our_set.add(s[i])
+            result = max(result, i + 1 - last)
 
-        return result if result != 10 ** 7 else 0
+        return result
 
 
-target = 7
-nums = [2,3,1,2,4,3]
-
+s = "pwwkew"
 if __name__ == '__main__':
     task = Solution()
-    print(task.minSubArrayLen(target, nums))
+    print(task.lengthOfLongestSubstring(s))

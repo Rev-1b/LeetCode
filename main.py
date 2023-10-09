c29c204be22b14e90c8dba981160f067a4a63c52
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-09 13:33:30 +0400

    Medium: Minimum Size Subarray Sum

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
 class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        def check_3Sum(collection, alter_coll_set):
-            for i in range(len(collection)):
-                for j in range(i + 1, len(collection)):
-                    target = -1 * (collection[i] + collection[j])
-                    if target in alter_coll_set:
-                        result.add(tuple(sorted([collection[i], collection[j], target])))
+    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+        result = 10 ** 7
+        prev_pointer = 0
+        curr_sum = 0
 
-        result = set()
-        negative, positive, zero = [], [], 0
-        for num in nums:
-            if num > 0:
-                positive.append(num)
-            elif num < 0:
-                negative.append(num)
-            else:
-                zero += 1
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            while curr_sum >= target:
+                curr_sum -= nums[prev_pointer]
+                result = min(result, i + 1 - prev_pointer)
+                prev_pointer += 1
 
-        neg_set, pos_set = set(negative), set(positive)
-        if zero:
-            for num in neg_set:
-                if -1 * num in pos_set:
-                    result.add((-1 * num, 0, num))
-
-        if zero >= 3:
-            result.add((0, 0, 0))
-
-        check_3Sum(negative, pos_set)
-        check_3Sum(positive, neg_set)
-
-        return result
+        return result if result != 10 ** 7 else 0
 
 
-nums = [-1, 0, 1, 2, -1, -4]
+target = 7
+nums = [2,3,1,2,4,3]
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.threeSum(nums))
+    print(task.minSubArrayLen(target, nums))

676dc752b1654971a6106bdb5bc9d2342d6ce2f4
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-10 13:05:37 +0400

    Medium: Valid Sudoku
    97 percent more effective than others

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+from collections import defaultdict
+
+
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        seen_chars = {}
-        left, result = 0, 0
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        all_chars = defaultdict(list)
+        for i in range(9):
+            for j in range(9):
+                char = board[i][j]
+                if char.isdigit():
+                    square_pos = (i // 3, j // 3)
+                    for pre_val in all_chars[char]:
+                        if i == pre_val[0][0] or j == pre_val[0][1] or square_pos == pre_val[1]:
+                            return False
+                    all_chars[char].append(((i, j), square_pos))
+        return True
 
-        for i in range(len(s)):
-            if s[i] in seen_chars:
-                if seen_chars[s[i]] >= left:
-                    left = seen_chars[s[i]] + 1
-            result = max(result, i + 1 - left)
-            seen_chars[s[i]] = i
-        return result
 
-
-s = " "
+board = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.lengthOfLongestSubstring(s))
+    print(task.isValidSudoku(board))

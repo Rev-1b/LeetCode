2e1b0cce200f4efdeb9719625c337a054bdf8b2b
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-12 14:22:57 +0400

    Easy: Valid Anagram
    Bruh it is one-liner(((
    Counter is OP!

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
+from collections import Counter
+
+
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        s_lst = s.split()
-        if len(pattern) != len(s_lst):
-            return False
-        pattern_map, heap = {}, set()
-        for char, word in zip(pattern, s_lst):
-            if char not in pattern_map and word in heap:
-                return False
-            tmp_res = pattern_map.setdefault(char, word)
-            if tmp_res != word:
-                return False
-            heap.add(word)
-        return True
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
 
 
-pattern = "abba"
-s = "dog dog dog dog"
+s = "anagram"
+t = "nagaram"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.wordPattern(pattern, s))
+    print(task.isAnagram(s, t))

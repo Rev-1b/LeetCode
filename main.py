05e9b6948cd2a8e135c95df494c17f6fcbc65e7d
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-12 14:03:34 +0400

    Easy: Isomorphic Strings

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 class Solution:
-    def canConstruct(self, ransomNote, magazine):
-        for i in set(ransomNote):
-            if magazine.count(i) < ransomNote.count(i):
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_main, t_main = {}, {}
+        for s_char, t_char in zip(s, t):
+            s_temp = s_main.setdefault(s_char, t_char)
+            t_temp = t_main.setdefault(t_char, s_char)
+            if s_temp != t_char or t_temp != s_char:
                 return False
         return True
 
 
-ransomNote = "aa"
-magazine = "aab"
+s = "aaq"
+t = "ooo"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.canConstruct(ransomNote, magazine))
+    print(task.isIsomorphic(s, t))

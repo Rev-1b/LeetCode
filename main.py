56de66ff6b8057f77c420e38bbd7f83646d43b28
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-15 14:09:27 +0400

    Easy: Valid Parentheses

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 class Solution:
-    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-        result = []
-        START, END = 0, 1
+    def isValid(self, s: str) -> bool:
+        closed = {')': '(', ']': '[', '}': '{'}
+        stack = ''
 
-        for elem in intervals:
-            if elem[END] < newInterval[START]:
-                result.append(elem)
-            elif elem[START] > newInterval[END]:
-                result.append(newInterval)
-                newInterval = elem
-            elif elem[START] <= newInterval[START] or elem[END] >= newInterval[END]:
-                newInterval = [min(elem[START], newInterval[START]),
-                               max(elem[END], newInterval[END])]
-            result.append(newInterval)
-            return result
+        for char in s:
+            if char in closed:
+                if stack and closed[char] == stack[-1]:
+                    stack = stack[:-1]
+                else:
+                    return False
+            else:
+                stack += char
+        return not stack
 
 
-intervals = [[1,3],[6,9]]
-newInterval = [2,5]
+s = "()[]{}(}"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.insert(intervals, newInterval))
+    print(task.isValid(s))
+

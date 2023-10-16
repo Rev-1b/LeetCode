ac42201778ae79633051e5c32fa6e31d420c5abf
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-16 16:21:48 +0400

    Medium: Copy List with Random Pointer

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 from typing import Optional
 
 
-class ListNode:
-    def __init__(self, x, next=None):
-        self.val = x
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
         self.next = next
+        self.random = random
 
 
 class Solution:
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = None
-        tail = None
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        old_head = head
+        new_head = Node(old_head.val)
+        new_tail = new_head
+        old_to_new = {old_head: new_head}
+        old_head = old_head.next
 
-        while l1 or l2:
-            if not l1:
-                obj = ListNode(l2.val)
-                l2 = l2.next if l2 else None
+        while old_head:
+            obj = Node(old_head.val)
+            old_to_new[old_head] = obj
+            new_tail.next = obj
+            new_tail = obj
+            old_head = old_head.next
 
-            elif not l2:
-                obj = ListNode(l1.val)
-                l1 = l1.next if l1 else None
+        while head:
+            old_to_new.get(head).random = old_to_new.get(head.random)
+            head = head.next
 
-            elif l1.val < l2.val:
-                obj = ListNode(l1.val)
-                l1 = l1.next if l1 else None
-            else:
-                obj = ListNode(l2.val)
-                l2 = l2.next if l2 else None
-
-            if head:
-                tail.next = obj
-                tail = obj
-            else:
-                head = obj
-                tail = obj
-
-        return head
+        return new_head
 
 
 if __name__ == '__main__':
-    num1 = ListNode(12)
-    num2 = ListNode(4, num1)
-    num3 = ListNode(1, num2)
-    num4 = ListNode(5)
-    num5 = ListNode(3, num4)
-    num6 = ListNode(1, num5)
 
     task = Solution()
-    head1 = task.mergeTwoLists(num3, num6)
+    head1 = task.copyRandomList()
     while head1:
         print(head1.val)
         head1 = head1.next

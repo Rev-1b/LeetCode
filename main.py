b8bd22bf543c01480d1aa86c255278c6798e8e0d
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-16 15:34:55 +0400

    Easy: Merge Two Sorted Lists

--- main.py
+++ main.py
@@ -8,43 +8,46 @@ class ListNode:
 
 
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         head = None
-        prev = None
-        prev_res = 0
+        tail = None
 
         while l1 or l2:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            num = (val1 + val2 + prev_res) % 10
-            prev_res = (val1 + val2 + prev_res) // 10
-            obj = ListNode(num)
+            if not l1:
+                obj = ListNode(l2.val)
+                l2 = l2.next if l2 else None
 
-            if not head:
-                head = obj
-                prev = head
+            elif not l2:
+                obj = ListNode(l1.val)
+                l1 = l1.next if l1 else None
+
+            elif l1.val < l2.val:
+                obj = ListNode(l1.val)
+                l1 = l1.next if l1 else None
             else:
-                prev.next = obj
-                prev = obj
+                obj = ListNode(l2.val)
+                l2 = l2.next if l2 else None
 
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            if head:
+                tail.next = obj
+                tail = obj
+            else:
+                head = obj
+                tail = obj
 
-        if prev_res:
-            prev.next = ListNode(prev_res)
         return head
 
 
 if __name__ == '__main__':
-    num1 = ListNode(1)
-    num2 = ListNode(0, num1)
-    num3 = ListNode(0, num2)
-    # num4 = ListNode(9)
-    num5 = ListNode(9)
-    num6 = ListNode(9, num5)
+    num1 = ListNode(12)
+    num2 = ListNode(4, num1)
+    num3 = ListNode(1, num2)
+    num4 = ListNode(5)
+    num5 = ListNode(3, num4)
+    num6 = ListNode(1, num5)
 
     task = Solution()
-    head1 = task.addTwoNumbers(num3, num6)
+    head1 = task.mergeTwoLists(num3, num6)
     while head1:
         print(head1.val)
         head1 = head1.next

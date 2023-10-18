9e73f611f8c378f6a9ad129749ceb7a0e5746696
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-18 15:45:09 +0400

    Medium: LRU Cache
    Much betta

--- main.py
+++ main.py
@@ -1,103 +1,23 @@
 from typing import Optional
-
-
-class ListNode:
-    def __init__(self, key: int, val: int):
-        self.key = key
-        self.val = val
-        self.next: Optional[ListNode] = None
-        self.prev: Optional[ListNode] = None
-
-    def __str__(self):
-        return f'val = {self.val}'
-
-
-class LinkedList:
-    def __init__(self):
-        self.head: Optional[ListNode] = None
-        self.tail: Optional[ListNode] = None
-        self.length = 0
-
-    def place_to_top(self, node: Optional[ListNode]):
-        if not node:
-            return
-
-        if not self.head:
-            self.head = node
-            self.tail = node
-        else:
-            node.next = self.head
-            self.head.prev = node
-            self.head = node
-        self.length += 1
-
-    def remove_node(self, node: Optional[ListNode]):
-        if not node:
-            return
-
-        if node.prev:
-            node.prev.next = node.next
-
-        if node.next:
-            node.next.prev = node.prev
-
-        if self.head == node:
-            self.head = node.next
-
-        if self.tail == node:
-            self.tail = node.prev
-
-        node.next = None
-        node.prev = None
-        self.length -= 1
+from collections import OrderedDict
 
 
 class LRUCache:
-    capacity: int
-    cache_map: dict[int, ListNode]
-    history: LinkedList
-
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache_map = {}
-        self.history = LinkedList()
+        self.data: OrderedDict[int, int] = OrderedDict()
+        self.capacity: int = capacity
 
     def get(self, key: int) -> int:
-        if key not in self.cache_map:
-            return -1
-
-        value_node: ListNode = self.cache_map[key]
-
-        if self.history.head != value_node:
-            self.history.remove_node(value_node)
-            self.history.place_to_top(value_node)
-
-        return value_node.val
+        return -1 if key not in self.data else self.data.setdefault(key, self.data.pop(key))
 
     def put(self, key: int, value: int) -> None:
-        value_node: ListNode = ListNode(key, value)
-
-        if key in self.cache_map:
-            self.remove_item(self.cache_map[key])
-
-        if len(self.cache_map) >= self.capacity:
-            self.evict_least_recent_item()
-
-        self.history.place_to_top(value_node)
-        self.cache_map[key] = value_node
-
-    def evict_least_recent_item(self) -> None:
-        lru_item: ListNode = self.history.tail
-
-        if lru_item is None:
-            return
-
-        self.remove_item(lru_item)
-
-    def remove_item(self, item: ListNode) -> None:
-        self.history.remove_node(item)
-        del self.cache_map[item.key]
-        del item
+        try:
+            self.data.move_to_end(key)
+            self.data[key] = value
+        except KeyError:
+            self.data[key] = value
+            if len(self.data) > self.capacity:
+                self.data.popitem(last=False)
 
 
 if __name__ == '__main__':

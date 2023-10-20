fce120587655e885e9c67d1afca1c64f727961e5
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-20 13:40:33 +0400

    Medium: Populating Next Right Pointers in Each Node II

--- main.py
+++ main.py
@@ -2,41 +2,56 @@ from typing import Optional
 from test import print_tree
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+class Node:
+    def __init__(self, val=0, left=None, right=None, next=None):
         self.val = val
         self.left = left
         self.right = right
+        self.next = next
 
     def __str__(self):
         return f'value = {self.val}'
 
 
 class Solution:
-    def buildTree(self, inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
-        if inorder:
-            value = postorder.pop()
-            value_pos = inorder.index(value)
-            node = TreeNode(value)
-            node.right = self.buildTree(inorder[value_pos + 1:], postorder)
-            node.left = self.buildTree(inorder[:value_pos], postorder)
-            return node
+    def connect(self, root: 'Node') -> 'Node':
+        if not root or not root.left and not root.right:
+            return root
+
+        storage = [root]
+        while storage:
+            temp_storage = []
+            prev = None
+            for node in storage:
+                if prev:
+                    prev.next = node
+                if node.left:
+                    temp_storage.append(node.left)
+                if node.right:
+                    temp_storage.append(node.right)
+                prev = node
+            storage = temp_storage
+        return root
 
 
 if __name__ == '__main__':
     task = Solution()
 
-    # node4 = TreeNode(7)
-    # node3 = TreeNode(15)
-    # node2 = TreeNode(20, node3, node4)
-    # node1 = TreeNode(9)
-    # root = TreeNode(3, node1, node2)
+    node8 = Node(5)
+    node7 = Node(23)
+    node6 = Node(1, node7, node8)
+    node5 = Node(6)
+    node4 = Node(7)
+    node3 = Node(15, node4, node5)
+    node2 = Node(20, None, node6)
+    node1 = Node(9, node3)
+    root = Node(3, node1, node2)
 
     inorder1 = [1, 2, 3, 4, 5, 6, 7, 9, 12, 13, 15, 20]
-    postorder1 = [1, 2, 4, 3, 6, 7, 5, 13, 20, 15, 12 ,9]
+    postorder1 = [1, 2, 4, 3, 6, 7, 5, 13, 20, 15, 12, 9]
 
     preorder2 = [1, 2]
     inorder2 = [2, 1]
 
-    root = task.buildTree(inorder1, postorder1)
+    root = task.connect(root)
     print_tree(root)

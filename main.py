b068fe713003d36c6d2c8c2a3fac664c43fab557
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-22 14:28:30 +0400

    Medium: Sum Root to Leaf Numbers

--- main.py
+++ main.py
@@ -13,36 +13,42 @@ class TreeNode:
 
 
 class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def do_magic(node: TreeNode, prev: str = '') -> None:
+            nonlocal result
+            curr_val = prev + str(node.val)
+            if not node.left and not node.right:
+                result += int(curr_val)
+            else:
+                if node.left:
+                    do_magic(node.left, curr_val)
+                if node.right:
+                    do_magic(node.right, curr_val)
+
+        do_magic(root)
+        return result
 
 
 if __name__ == '__main__':
     task = Solution()
 
-    # node1 = TreeNode(1)
-    # node2 = TreeNode(2, node1)
-    # node4 = TreeNode(4)
-    # node3 = TreeNode(3, node2, node4)
-    # node6 = TreeNode(6)
-    # node7 = TreeNode(7, node6)
-    # node5 = TreeNode(5, node3, node7)
-    # node12 = TreeNode(12)
-    # node10 = TreeNode(10)
-    # node11 = TreeNode(11, node10, node12)
-    # node9 = TreeNode(9, None, node11)
-    # node8 = TreeNode(8, node5, node9)
-    #
-    # inorder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    # postorder = [1, 2, 4, 3, 6, 7, 5, 10, 12, 11, 9]
-    # preorder = [8, 5, 3, 2, 1, 4, 7, 6, 9, 11, 10, 12]
+    node1 = TreeNode(1)
+    node2 = TreeNode(2, node1)
+    node4 = TreeNode(4)
+    node3 = TreeNode(3, node2, node4)
+    node6 = TreeNode(6)
+    node7 = TreeNode(7, node6)
+    node5 = TreeNode(5, node3, node7)
+    node12 = TreeNode(12)
+    node10 = TreeNode(10)
+    node11 = TreeNode(11, node10, node12)
+    node9 = TreeNode(9, None, node11)
+    node8 = TreeNode(8, node5, node9)
 
-    # print_tree(node8)
-    # print('\n\n------------------------------------------------\n\n')
-    print(task.hasPathSum(node8, 3))
-    # print_tree(node8)
+    inorder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    postorder = [1, 2, 4, 3, 6, 7, 5, 10, 12, 11, 9]
+    preorder = [8, 5, 3, 2, 1, 4, 7, 6, 9, 11, 10, 12]
+
+    print(task.sumNumbers(node8))

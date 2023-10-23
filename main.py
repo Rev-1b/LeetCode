7b02e7fdb341e562d9b39444db11d6d1e12c8d0a
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-23 16:39:50 +0400

    Medium:  Binary Search Tree Iterator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from test import print_tree
+from typing import Optional, Generator
+from collections import deque
 
 
 class TreeNode:
@@ -12,21 +12,27 @@ class TreeNode:
         return f'value = {self.val}'
 
 
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None or root == p or root == q:
-            return root
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        if left and right:
-            return root
-        if not left:
-            return right
-        return left
+class BSTIterator:
+    def __init__(self, root: Optional[TreeNode]):
+        self.generator = self.do_magic(root)
+        self.answer = next(self.generator, None)
+
+    def next(self) -> int:
+        answer, self.answer = self.answer, next(self.generator, None)
+        return answer
+
+    def hasNext(self) -> bool:
+        return bool(self.answer)
+
+    def do_magic(self, node: TreeNode) -> Generator[int, None, None]:
+        if node:
+            yield from self.do_magic(node.left)
+            yield node.val
+            yield from self.do_magic(node.right)
 
 
 if __name__ == '__main__':
-    task = Solution()
+    # task = Solution()
 
     node1 = TreeNode(1)
     node2 = TreeNode(2, node1)
@@ -41,8 +47,16 @@ if __name__ == '__main__':
     node9 = TreeNode(9, None, node11)
     node8 = TreeNode(8, node5, node9)
 
-    inorder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    postorder = [1, 2, 4, 3, 6, 7, 5, 10, 12, 11, 9]
-    preorder = [8, 5, 3, 2, 1, 4, 7, 6, 9, 11, 10, 12]
+    # inorder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    # postorder = [1, 2, 4, 3, 6, 7, 5, 10, 12, 11, 9]
+    # preorder = [8, 5, 3, 2, 1, 4, 7, 6, 9, 11, 10, 12]
 
-    print(task.lowestCommonAncestor(node8, node10, node12))
+    # print(task.lowestCommonAncestor(node8, node10, node12))
+
+    obj = BSTIterator(node8)
+    for _ in range(12):
+        print(obj.hasNext())
+        print(obj.next())
+        print(obj.hasNext(), end='\n\n')
+
+

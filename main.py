7762816e3965fc7ac36ddc8e8c0821127930740e
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-10-29 15:40:22 +0400

    Medium: Jump Game

--- main.py
+++ main.py
@@ -1,42 +1,20 @@
-class Node:
-    def __init__(self, children=None):
-        self.children = children if children is not None else {}
-        self.is_word = False
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        target = nums[-1]
 
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] + i >= target:
+                target = i
 
-class WordDictionary:
-
-    def __init__(self):
-        self.root = Node()
-
-    def addWord(self, word: str) -> None:
-        curr = self.root
-        for letter in word:
-            curr = curr.children.setdefault(letter, Node())
-        curr.is_word = True
-
-    def search(self, word: str) -> bool:
-        def do_magic(node: Node, index: int) -> bool:
-            if index == len(word):
-                return node.is_word
-            if word[index] == '.':
-                for child in node.children.values():
-                    res = do_magic(child, index + 1)
-                    if res:
-                        return True
-            if word[index] in node.children:
-                return do_magic(node.children[word[index]], index + 1)
+        if target == 0:
+            return True
+        else:
             return False
-
-        return do_magic(self.root, 0)
 
 
 if __name__ == '__main__':
-    task = WordDictionary()
-    task.addWord("bad")
-    task.addWord("dad")
-    task.addWord("mad")
-    print(task.search("pad"))
-    print(task.search("bad"))
-    print(task.search(".ad"))
-    print(task.search("b.."))
+    task = Solution()
+
+    nums = [2, 3, 1, 1, 4]
+
+    print(task.canJump(nums))

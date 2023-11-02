72ccbb7eee9ad0ab8f53de47bf91cdfccb8848a9
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-02 12:15:16 +0400

    Medium:  Maximum Subarray

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-class Node:
-    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
-        self.val = val
-        self.isLeaf = isLeaf
-        self.topLeft = topLeft
-        self.topRight = topRight
-        self.bottomLeft = bottomLeft
-        self.bottomRight = bottomRight
-
-
 class Solution:
-    def construct(self, grid: list[list[int]]) -> Node:
-        n = len(grid)
-        amount = sum(grid[i][j] for i in range(n) for j in range(n))
-        if amount == n ** 2:
-            return Node(True, True, None, None, None, None)
-        if not amount:
-            return Node(False, True, None, None, None, None)
+    def maxSubArray(self, nums: list[int]) -> int:
+        max_sum = float('-inf')
+        curr_sum = 0
 
-        top = grid[:n // 2]
-        bottom = grid[n // 2:]
-        top_left = self.construct([row[:n // 2] for row in top])
-        top_right = self.construct([row[n // 2:] for row in top])
-        bottom_left = self.construct([row[:n // 2] for row in bottom])
-        bottom_right = self.construct([row[n // 2:] for row in bottom])
-        return Node(True, False, top_left, top_right, bottom_left, bottom_right)
+        for num in nums:
+            curr_sum += num
+
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+
+            if curr_sum < 0:
+                curr_sum = 0
+
+        return max_sum
 
 
 if __name__ == '__main__':
     task = Solution()
 
-    grid1 = [[1, 1, 1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 1, 1, 1, 1, 1],
-             [1, 1, 1, 1, 1, 1, 1, 1],
-             [1, 1, 1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 1, 0, 0, 0, 0]]
+    nums1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-    print(task.construct(grid1))
+    print(task.maxSubArray(nums1))

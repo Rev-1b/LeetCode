bed96d721d33670c2b786f19cba43fb3a7dc787f
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-04 14:12:54 +0400

    Medium: Find Minimum in Rotated Sorted Array

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def findMin(self, nums: list[int]) -> int:
         left, right = 0, len(nums) - 1
 
         while left <= right:
+            if nums[left] <= nums[right]:
+                return nums[left]
             mid = (left + right) // 2
-
-            if nums[mid] == target:
-                return mid
-
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target < nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-
+            if nums[mid] < nums[left]:
+                right = mid
             else:
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-
-        return -1
+                left = mid + 1
 
 
 if __name__ == '__main__':
     task = Solution()
 
-    nums = [3,1]
-    target = 0
+    nums = [3,1,2]
 
-    print(task.search(nums, target))
+    print(task.findMin(nums))

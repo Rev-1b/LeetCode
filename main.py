1471f543d15b362f3c9bb32fbce8567650eea258
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-05 19:04:50 +0400

    Medium: H-Index

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 class Solution:
-    def findMin(self, nums: list[int]) -> int:
-        left, right = 0, len(nums) - 1
-
-        while left <= right:
-            if nums[left] <= nums[right]:
-                return nums[left]
-            mid = (left + right) // 2
-            if nums[mid] < nums[left]:
-                right = mid
-            else:
-                left = mid + 1
+    def hIndex(self, citations: list[int]) -> int:
+        citations.sort()
+        i = 1
+        while i <= len(citations):
+            if citations[-i] < i:
+                return i - 1
+            i += 1
+        return i - 1
 
 
 if __name__ == '__main__':
     task = Solution()
 
-    nums = [3,1,2]
+    nums = [3, 2, 1, 5, 6, 4]
 
-    print(task.findMin(nums))
+
+    print(task.hIndex(nums))

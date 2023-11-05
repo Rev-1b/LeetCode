fe89e2de664c901a8e72a1de1d56c8bb9738bcc7
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-05 19:06:32 +0400

    Medium: Product of Array Except Self

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-from random import choice
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        answer = [1] * (len(nums))
 
+        prefix = 1
+        for i in range(len(nums)):
+            answer[i] = prefix
+            prefix *= nums[i]
 
-class RandomizedSet:
+        postfix = 1
+        for j in range(len(nums) - 1, -1, -1):
+            answer[j] *= postfix
+            postfix *= nums[j]
 
-    def __init__(self):
-        self.data_map = {}
-        self.data_list = []
+        return answer
 
-    def insert(self, val: int) -> bool:
-        if val in self.data_map:
-            return False
-        self.data_list.append(val)
-        self.data_map[val] = len(self.data_list) - 1
-        return True
-
-    def remove(self, val: int) -> bool:
-        if val not in self.data_map:
-            return False
-
-        last_elem = self.data_list[-1]
-        val_indx = self.data_map[val]
-        self.data_map[last_elem] = val_indx
-        self.data_list[val_indx] = last_elem
-
-        self.data_list[-1] = val
-        self.data_list.pop()
-        self.data_map.pop(val)
-        return True
-
-    def getRandom(self) -> int:
-        return choice(self.data_list)
 
 if __name__ == '__main__':
-    # task = Solution()
-    #
-    # nums = [3, 2, 1, 5, 6, 4]
-    #
-    #
-    # print(task.hIndex(nums))
-    pass
+    task = Solution()
+
+    nums = [3, 2, 1, 5, 6, 4]
+
+    print(task.productExceptSelf(nums))

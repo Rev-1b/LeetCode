7c10d6f7b296807bdb477e406d3cf79562f6b492
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-05 19:05:39 +0400

    Medium: Insert Delete GetRandom O(1)

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-class Solution:
-    def hIndex(self, citations: list[int]) -> int:
-        citations.sort()
-        i = 1
-        while i <= len(citations):
-            if citations[-i] < i:
-                return i - 1
-            i += 1
-        return i - 1
+from random import choice
 
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.data_map = {}
+        self.data_list = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.data_map:
+            return False
+        self.data_list.append(val)
+        self.data_map[val] = len(self.data_list) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.data_map:
+            return False
+
+        last_elem = self.data_list[-1]
+        val_indx = self.data_map[val]
+        self.data_map[last_elem] = val_indx
+        self.data_list[val_indx] = last_elem
+
+        self.data_list[-1] = val
+        self.data_list.pop()
+        self.data_map.pop(val)
+        return True
+
+    def getRandom(self) -> int:
+        return choice(self.data_list)
 
 if __name__ == '__main__':
-    task = Solution()
-
-    nums = [3, 2, 1, 5, 6, 4]
-
-
-    print(task.hIndex(nums))
+    # task = Solution()
+    #
+    # nums = [3, 2, 1, 5, 6, 4]
+    #
+    #
+    # print(task.hIndex(nums))
+    pass

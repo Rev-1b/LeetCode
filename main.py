a1de3822f1c0e19abc007d723d57e0c11f344a54
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-20 21:02:29 +0400

    Easy: Add Binary

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import heapq
-
-
 class Solution:
-    def kSmallestPairs(self, nums1: list[int], nums2: list[int], k: int) -> list[list[int]]:
-        heap = []
-        heapq.heapify(heap)
+    def addBinary(self, a: str, b: str) -> str:
+        additional = 0
+        a_pointer, b_pointer = len(a) - 1, len(b) - 1
 
-        for i in range(min(len(nums1), k)):
-            heapq.heappush(heap, (nums1[i] + nums2[0], nums1[i], nums2[0], 0))
+        res = ''
+        while a_pointer >= 0 or b_pointer >= 0 or additional:
+            if a_pointer >= 0:
+                additional += int(a[a_pointer])
 
-        result = []
-        while k > 0 and heap:
-            _, num1, num2, index = heapq.heappop(heap)
-            result.append((num1, num2))
+            if b_pointer >= 0:
+                additional += int(b[b_pointer])
 
-            if index + 1 < len(nums2):
-                heapq.heappush(heap, (num1 + nums2[index + 1], num1, nums2[index + 1], index + 1))
+            res = str(additional % 2) + res
+            additional //= 2
 
-            k -= 1
+            a_pointer -= 1
+            b_pointer -= 1
 
-        return result
+        return res
 
 
-nums1 = [1, 2]
-nums2 = [3]
-k = 2
+
+
+
+a = "1010"
+b = "1011"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.kSmallestPairs(nums1, nums2, k))
+    print(task.addBinary(a, b))

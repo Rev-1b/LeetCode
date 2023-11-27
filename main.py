143495ff5837982262dde4216f5711b99fc4504b
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-27 17:14:13 +0400

    Medium: Coin Change

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 class Solution:
-    def coinChange(self, coins: list[int], amount: int) -> int:
-        cache = [amount + 1] * (amount + 1)
-        cache[0] = 0
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        total_number = len(nums)
+        dp = [0 for _ in range(total_number)]
+        for i in range(1, total_number):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp) + 1
 
-        for i in range(amount + 1):
-            for coin in coins:
-                if coin <= i:
-                    cache[i] = min(cache[i], cache[i - coin] + 1)
-        return -1 if cache[amount] == amount + 1 else cache[amount]
-
-
-coins = [1, 2, 5]
-amount = 11
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.coinChange(coins, amount))

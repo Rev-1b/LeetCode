f551c4c2127a96b11b72632833793e4681743e63
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-06 18:59:19 +0400

    Medium: Triangle

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 class Solution:
-    def lengthOfLIS(self, nums: list[int]) -> int:
-        total_number = len(nums)
-        dp = [0 for _ in range(total_number)]
-        for i in range(1, total_number):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp) + 1
+    def minimumTotal(self, triangle: list[list[int]]) -> int:
+        i = 1
 
+        while i < len(triangle):
+            row = triangle[i]
+            j = 0
+
+            while j < len(row):
+                if j == 0:
+                    triangle[i][j] = triangle[i - 1][0] + triangle[i][j]
+
+                elif j == len(row) - 1:
+                    triangle[i][j] = triangle[i - 1][len(row) - 2] + triangle[i][j]
+
+                else:
+                    triangle[i][j] = min(triangle[i - 1][j] + triangle[i][j],
+                                         triangle[i - 1][j - 1] + triangle[i][j])
+
+                j += 1
+            i += 1
+
+        return min(triangle[-1])
+
+
+triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
 
 if __name__ == '__main__':
     task = Solution()
+    print(task.minimumTotal(triangle))

d277fd22dc13c5d419aaeb89bdfd26cad20a79c1
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-08 18:05:33 +0400

    Medium: Longest Palindrome
    bad solution

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
+from collections import defaultdict
+
+
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
-        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
-            return 0
+    def longestPalindrome(self, s: str) -> str:
 
-        start_i, start_j = 1, 0
-        n, m = len(obstacleGrid), len(obstacleGrid[0])
+        cache = defaultdict(list)
+        longest_palindrome = s[0]
 
-        if n == 1 and m == 1:
-            return 1
+        def is_palindrome(word):
+            if len(word) in [0, 1]:
+                return True
 
-        if n == 1:
-            temp = sum(obstacleGrid[0])
-            return 1 if temp == 0 else 5
+            if word[0] != word[-1]:
+                return False
 
-        obstacleGrid[0][0] = 1
-
-        while True:
-            i, j = start_i, start_j
-
-            while j < m and i >= 0:
-                if obstacleGrid[i][j] == 1:
-                    obstacleGrid[i][j] = None
-                else:
-                    left = obstacleGrid[i][j - 1] if j > 0 and obstacleGrid[i][j - 1] is not None else 0
-                    top = obstacleGrid[i - 1][j] if i > 0 and obstacleGrid[i - 1][j] is not None else 0
-
-                    obstacleGrid[i][j] = left + top
-
-                if i == n - 1 and j == m - 1:
-                    return obstacleGrid[i][j]
-
-                i -= 1
-                j += 1
-
-            if start_i < n - 1:
-                start_i += 1
             else:
-                start_j += 1
+                return is_palindrome(word[1:-1])
+
+        for index, char in enumerate(s):
+            if cache[char]:
+                lst = cache[char]
+                for i in lst:
+                    if len(longest_palindrome) <= index - i:
+                        res = is_palindrome(s[i:index + 1])
+                        if res:
+                            longest_palindrome = s[i:index + 1]
+                            break
+                    else:
+                        break
+
+            cache[char].append(index)
+
+        return longest_palindrome
 
 
-obstacleGrid = [[0, 0]]
+s = "aacabdkacaa"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.uniquePathsWithObstacles(obstacleGrid))
+    print(task.longestPalindrome(s))

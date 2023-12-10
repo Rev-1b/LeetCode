d25fbcf9d271c9d8704fab4299760cc65044e1ad
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-10 20:30:55 +0400

    Medium:  Interleaving String
    Test version

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
-from collections import defaultdict
-
-
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n, m, l = len(s1), len(s2), len(s3)
 
-        cache = defaultdict(list)
-        longest_palindrome = s[0]
+        if n + m != l:
+            return False
 
-        def is_palindrome(word):
-            if len(word) in [0, 1]:
-                return True
+        cache = [[False] * (m + 1) for _ in range(n + 1)]
+        cache[0][0] = True
 
-            if word[0] != word[-1]:
-                return False
+        for j in range(1, m + 1):
+            cache[0][j] = cache[0][j - 1] and s2[j - 1] == s3[j - 1]
 
-            else:
-                return is_palindrome(word[1:-1])
+        for i in range(1, n + 1):
+            cache[i][0] = cache[i - 1][0] and s1[i - 1] == s3[i - 1]
 
-        for index, char in enumerate(s):
-            if cache[char]:
-                lst = cache[char]
-                for i in lst:
-                    if len(longest_palindrome) <= index - i:
-                        res = is_palindrome(s[i:index + 1])
-                        if res:
-                            longest_palindrome = s[i:index + 1]
-                            break
-                    else:
-                        break
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                s1_prev = cache[i - 1][j]
+                s1_look = s1[i - 1] == s3[i + j - 1]
 
-            cache[char].append(index)
+                s2_prev = cache[i][j - 1]
+                s2_look = s2[j - 1] == s3[i + j - 1]
 
-        return longest_palindrome
+                cache[i][j] = (cache[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
+                              (cache[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+
+        return cache[n][m]
 
 
-s = "aacabdkacaa"
+s1 = "aabcc"
+s2 = "dbbca"
+s3 = "aadbbcbcac"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.longestPalindrome(s))
+    print(task.isInterleave(s1, s2, s3))

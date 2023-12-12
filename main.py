0c7f80b436a99ec020f99b1a8518868b34f71309
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-12 19:45:50 +0400

    Medium: Edit Distance

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 class Solution:
-    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        n, m, l = len(s1), len(s2), len(s3)
-
-        if n + m != l:
-            return False
-
-        cache = [[False] * (m + 1) for _ in range(n + 1)]
-        cache[0][0] = True
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m = len(word1), len(word2)
+        cache = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
 
         for j in range(1, m + 1):
-            cache[0][j] = cache[0][j - 1] and s2[j - 1] == s3[j - 1]
+            cache[0][j] = j
 
         for i in range(1, n + 1):
-            cache[i][0] = cache[i - 1][0] and s1[i - 1] == s3[i - 1]
+            cache[i][0] = i
 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
-                cache[i][j] = (cache[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
-                              (cache[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+                if word1[i - 1] == word2[j - 1]:
+                    cache[i][j] = cache[i - 1][j - 1]
+
+                else:
+                    cache[i][j] = 1 + min(cache[i - 1][j - 1],
+                                          cache[i][j - 1],
+                                          cache[i - 1][j])
 
         return cache[n][m]
 
 
-s1 = "aabcc"
-s2 = "dbbca"
-s3 = "aadbbcbcac"
+word1 = "рупор"
+word2 = "р"
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.isInterleave(s1, s2, s3))
+    print(task.minDistance(word1, word2))

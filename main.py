ea7a67255c2c7146f03bf66062c5aeaddbd456a6
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-13 15:52:40 +0400

    New Cycle
    Easy: Merge Sorted Array

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 class Solution:
-    def maximalSquare(self, matrix: list[list[str]]) -> int:
-        n, m = len(matrix), len(matrix[0])
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        back = n + m - 1
 
-        if all(i == '0' for row in matrix for i in row):
-            return 0
+        n -= 1
+        m -= 1
 
-        result = 1
+        while n >= 0:
+            if m >= 0 and nums1[m] > nums2[n]:
+                nums1[back] = nums1[m]
+                m -= 1
+            else:
+                nums1[back] = nums2[n]
+                n -= 1
+            back -= 1
 
-        for i in range(1, n):
-            for j in range(1, m):
-                if matrix[i][j] == '1':
-                    a, b, c = map(int, (matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]))
-                    if a and b and c:
-                        res = min(a, b, c) + 1
-                        matrix[i][j] = str(res)
-                        result = max(res, result)
+        return nums1
 
-        return result ** 2
-
-
-matrix = [["1", "1", "1", "1", "0"],
-          ["1", "1", "1", "1", "0"],
-          ["1", "1", "1", "1", "1"],
-          ["1", "1", "1", "1", "1"],
-          ["0", "0", "1", "1", "1"]]
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.maximalSquare(matrix))
+    print(task.merge(nums1=[0, 0, 0, 0, 0], m=0, nums2=[1, 2, 3, 4, 5], n=3))

204754c6aaa613fbb97f158f3d8bc2c849d2c3ff
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-13 16:11:36 +0400

    Easy: Remove Element

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 class Solution:
-    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-        back = n + m - 1
+    def removeElement(self, nums: list[int], val: int) -> int:
+        left = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[left] = nums[i]
+                left += 1
 
-        n -= 1
-        m -= 1
-
-        while n >= 0:
-            if m >= 0 and nums1[m] > nums2[n]:
-                nums1[back] = nums1[m]
-                m -= 1
-            else:
-                nums1[back] = nums2[n]
-                n -= 1
-            back -= 1
-
-        return nums1
+        return left
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.merge(nums1=[0, 0, 0, 0, 0], m=0, nums2=[1, 2, 3, 4, 5], n=3))
+    print(task.removeElement(nums=[0, 1, 2, 2, 3, 0, 4, 2], val=2))

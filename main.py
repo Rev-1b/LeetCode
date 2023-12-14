f5102e925c9b6a8cb9e4bbda5461a0c3e97ed180
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-14 16:06:03 +0400

    Medium: Jump Game II

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 class Solution:
-    def canJump(self, nums: list[int]) -> bool:
-        jump = nums[0]
+    def jump(self, nums: list[int]) -> int:
+        end, res, curr = 0, 0, 0
 
-        for num in nums[1:]:
-            if not jump:
-                return False
-            jump = max(jump - 1, num)
-        return True
+        for i in range(len(nums) - 1):
+            curr = max(curr, i + nums[i])
+            if curr >= len(nums) - 1:
+                res += 1
+                return res
+
+            if i == end:
+                res += 1
+                end = curr
+
+        return res
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.canJump(nums=[3, 2, 1, 0, 4]))
+    print(task.jump(nums=[2, 3, 1, 1, 4]))

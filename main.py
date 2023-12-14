5441e59bf349ee38823adf042fcde22f3f2afe68
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-14 14:42:18 +0400

    Easy: Best Time to Buy and Sell Stock

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+    def maxProfit(self, prices: list[int]) -> int:
+        res = 0
+
+        slow = prices[0]
+        for num in prices[1:]:
+            if slow < num:
+                res = max(res, num - slow)
+            else:
+                slow = num
+        return res
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.majorityElement(nums=[2, 2, 1, 1, 1, 2, 2]))
+    print(task.maxProfit(prices=[7, 6, 4, 3, 1]))

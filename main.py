24d379136b984c37cfd902222fb60aa49077be43
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-14 14:56:31 +0400

    Medium: Best Time to Buy and Sell Stock II

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
+        slow = prices[0]
         res = 0
 
-        slow = prices[0]
         for num in prices[1:]:
             if slow < num:
-                res = max(res, num - slow)
-            else:
-                slow = num
+                res += num - slow
+            slow = num
+
         return res
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.maxProfit(prices=[7, 6, 4, 3, 1]))
+    print(task.maxProfit(prices=[7, 1, 5, 3, 6, 4]))

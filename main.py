fa59455d82f4d856973e772c09e305bf808f31fa
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-14 15:15:11 +0400

    Medium: Jump Game

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
-        slow = prices[0]
-        res = 0
+    def canJump(self, nums: list[int]) -> bool:
+        jump = nums[0]
 
-        for num in prices[1:]:
-            if slow < num:
-                res += num - slow
-            slow = num
-
-        return res
+        for num in nums[1:]:
+            if not jump:
+                return False
+            jump = max(jump - 1, num)
+        return True
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.maxProfit(prices=[7, 1, 5, 3, 6, 4]))
+    print(task.canJump(nums=[3, 2, 1, 0, 4]))

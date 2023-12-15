543931ce68c1a2580e74e54eadf1db4fe165d23b
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-15 18:58:34 +0400

    Medium: Gas Station

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 class Solution:
-    def productExceptSelf(self, nums: list[int]) -> list[int]:
-        result = [1 for _ in range(len(nums))]
+    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
 
-        prefix = 1
-        for i in range(len(nums)):
-            result[i] *= prefix
-            prefix *= nums[i]
-
-        postfix = 1
-        for j in range(len(nums) - 1, -1, -1):
-            result[j] *= postfix
-            postfix *= nums[j]
-
+        fuel, result = 0, 0
+        for i in range(len(gas)):
+            fuel += gas[i] - cost[i]
+            if fuel < 0:
+                fuel = 0
+                result = i + 1
         return result
+
+
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.productExceptSelf(nums=[1, 2, 3, 4, 5]))
+    print(task.canCompleteCircuit(gas=[1, 2, 3, 4, 5], cost=[3, 4, 5, 1, 2]))

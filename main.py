fc2501e8ca688a38d0bf3f4ac09f30ba07aebc38
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-15 19:07:52 +0400

    Medium: Reverse Words in a String

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 class Solution:
-    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
-        if sum(gas) < sum(cost):
-            return -1
-
-        fuel, result = 0, 0
-        for i in range(len(gas)):
-            fuel += gas[i] - cost[i]
-            if fuel < 0:
-                fuel = 0
-                result = i + 1
-        return result
-
-
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(reversed(s.split()))
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.canCompleteCircuit(gas=[1, 2, 3, 4, 5], cost=[3, 4, 5, 1, 2]))
+    print(task.reverseWords(s=" the sky is blue "))

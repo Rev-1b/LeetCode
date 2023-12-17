61ae1f5a4a96feb8dc17f9add2445bcc168950cc
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-17 17:26:48 +0400

    Medium: Zigzag Conversion

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 class Solution:
-    def reverseWords(self, s: str) -> str:
-        return ' '.join(reversed(s.split()))
+    def convert(self, s: str, numb: int) -> str:
+        if numb == 1:
+            return s
 
+        storage = ['' for _ in range(numb)]
+
+        pointer = 0
+        mult = 1
+        for char in s:
+            storage[pointer] += char
+
+            if pointer == numb - 1:
+                mult = -1
+
+            if pointer == 0:
+                mult = 1
+
+            pointer += mult
+
+        return ''.join(storage)
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.reverseWords(s=" the sky is blue "))
+    print(task.convert(s="PAYPALISHIRING", numb=3))

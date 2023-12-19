e79d6602abec77c480dd9c113f781ce8a5191bb2
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-19 19:58:12 +0400

    Medium: Minimum Size Subarray Sum

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
 class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        pos_list, neg_list, zero = [], [], 0
-        result = set()
+    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+        result = float('inf')
+        curr_sum, index = 0, 0
 
-        for elem in nums:
-            if elem < 0:
-                neg_list.append(elem)
-            elif elem > 0:
-                pos_list.append(elem)
-            else:
-                zero += 1
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            while curr_sum >= target:
+                curr_sum -= nums[index]
+                result = min(result, i + 1 - index)
+                index += 1
 
-        pos_set, neg_set = set(pos_list), set(neg_list)
+        return result if result != float('-inf') else 0
 
-        if zero >= 3:
-            result.add((0, 0, 0))
-
-        if zero:
-            for pos_num in pos_set:
-                if -1 * pos_num in neg_set:
-                    result.add((-1 * pos_num, 0, pos_num))
-
-        def do_magic(num_list: list[int], num_set: set[int]) -> None:
-            for i in range(len(num_list)):
-                for j in range(i + 1, len(num_list)):
-                    target = -1 * (num_list[i] + num_list[j])
-                    if target in num_set:
-                        result.add(tuple(sorted([num_list[i], target, num_list[j]])))
-
-        do_magic(pos_list, neg_set)
-        do_magic(neg_list, pos_set)
-
-        return result
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.threeSum(nums=[-1, 0, 1, 2, -1, -4]))
+    print(task.minSubArrayLen(target=7, nums=[2, 3, 1, 2, 4, 3]))

d2fa29e69e7b62964c67b8382a5aac670e585f18
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-26 15:13:02 +0400

    Medium: Spiral Matrix

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-from collections import defaultdict
-
-
 class Solution:
-    def isValidSudoku(self, board: list[list[str]]) -> bool:
-        cache = defaultdict(list)
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        result = [matrix[0][0]]
+        step = 0
+        direction = 0
+        i, j = 0, 0
+        n, m = len(matrix), len(matrix[0])
 
-        for i in range(9):
-            for j in range(9):
-                elem = board[i][j]
-                if elem.isdigit():
-                    group = (i // 3, j // 3)
-                    if elem in cache:
-                        for coords in cache[elem]:
-                            i1, j1 = coords
-                            if i1 == i or j1 == j or (i1 // 3, j1 // 3) == group:
-                                return False
-                    cache[elem].append((i, j))
+        while len(result) < n * m:
+            if direction == 0:
+                if j + step < m - 1:
+                    j += 1
+                else:
+                    direction += 1
+                    continue
+            elif direction == 1:
+                if i + step < n - 1:
+                    i += 1
+                else:
+                    direction += 1
+                    continue
+            elif direction == 2:
+                if j > step:
+                    j -= 1
+                else:
+                    direction += 1
+                    continue
+            elif direction == 3:
+                if i >= step + 2:
+                    i -= 1
+                else:
+                    direction = 0
+                    step += 1
+                    continue
 
-        return True
+            result.append(matrix[i][j])
 
+        return result
 
-board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
-         ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-         ["3", "9", "8", ".", ".", ".", ".", "6", "."],
-         ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-         ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-         ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-         [".", "6", ".", ".", ".", ".", "2", "8", "."],
-         [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-         [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.isValidSudoku(board))
+    print(task.spiralOrder(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]))

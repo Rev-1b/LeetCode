f942a9a6d027b30f2ad59020781306efa5b96220
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-26 16:33:51 +0400

    Medium:  Game of Life

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 class Solution:
-    def setZeroes(self, matrix: list[list[int]]) -> None:
-        rows, cols = set(), set()
+    def gameOfLife(self, board: list[list[int]]) -> None:
+        n, m = len(board), len(board[0])
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
+        for i in range(n):
+            for j in range(m):
+                living_cells = 0
 
-        for row in rows:
-            matrix[row] = [0] * len(matrix[0])
+                i_start = i - 1 if i > 1 else 0
+                j_start = j - 1 if j > 1 else 0
+                i_end = i + 2 if i < n - 1 else n
+                j_end = j + 2 if j < m - 1 else m
 
-        for col in cols:
-            for i in range(len(matrix)):
-                matrix[i][col] = 0
+                for k in range(i_start, i_end):
+                    for q in range(j_start, j_end):
+                        if not (k == i and q == j) and board[k][q] in (1, 'D'):
+                            living_cells += 1
+
+                if board[i][j] == 1 and not 2 <= living_cells <= 3:
+                    board[i][j] = 'D'
+                if board[i][j] == 0 and living_cells == 3:
+                    board[i][j] = 'A'
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == 'D':
+                    board[i][j] = 0
+                if board[i][j] == 'A':
+                    board[i][j] = 1
+
+        return board
 
 
-matrix = [[1, 1, 1],
-          [1, 0, 1],
-          [1, 1, 1]]
+board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.setZeroes(matrix=matrix))
-    for row in matrix:
-        print(*row)
+    print(task.gameOfLife(board))

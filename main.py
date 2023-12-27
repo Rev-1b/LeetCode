fb6c7346411196b5152bf2839689773cd9ad9244
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-27 16:57:52 +0400

    Medium: Contains Duplicate II

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+from collections import defaultdict
+
+
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        cache = set()
-        while n != 1:
-            if n in cache:
-                return False
-            cache.add(n)
-            temp = 0
-            for digit in str(n):
-                temp += int(digit) ** 2
-            n = temp
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        cache = defaultdict(list)
 
-        return True
-
+        for index, num in enumerate(nums):
+            if num in cache and index - cache[num][-1] <= k:
+                return True
+            cache[num].append(index)
+        return False
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.isHappy(n=19))
+    print(task.containsNearbyDuplicate(nums=[1, 2, 3, 5, 1], k=3))

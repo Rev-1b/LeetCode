643f40044e7253b807de8f4b9ec5da0fa4b37e2a
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-27 16:44:46 +0400

    Medium: Two Sum

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-Group Anagrams
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        cache = {}
+
+        for index, num in enumerate(nums):
+            aim = target - num
+            if aim in cache:
+                return cache[aim], index
+            else:
+                cache[num] = index
+
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
+    print(task.twoSum(nums=[2, 7, 11, 15], target=9))

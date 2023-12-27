2c2cafa29758180937064db6a24883d28d92d8f8
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-27 17:12:00 +0400

    Medium:  Longest Consecutive Sequence

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from collections import defaultdict
-
-
 class Solution:
-    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        cache = defaultdict(list)
+    def longestConsecutive(self, nums: list[int]) -> int:
+        if not len(nums):
+            return 0
 
-        for index, num in enumerate(nums):
-            if num in cache and index - cache[num][-1] <= k:
-                return True
-            cache[num].append(index)
-        return False
+        cache = set(nums)
+        result = 1
+        temp = 1
+        curr = cache.pop()
+
+        while cache:
+            if curr - 1 in cache:
+                cache.add(curr)
+                curr -= 1
+
+            elif curr + 1 in cache:
+                temp += 1
+                result = max(result, temp)
+                if curr in cache:
+                    cache.remove(curr)
+                curr += 1
+
+            else:
+                temp = 1
+                curr = cache.pop()
+
+        return result
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.containsNearbyDuplicate(nums=[1, 2, 3, 5, 1], k=3))
+    print(task.longestConsecutive(nums=[0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
+
+    nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+    print(sorted(nums))

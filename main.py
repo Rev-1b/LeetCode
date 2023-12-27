9a94f89d8ca64e119915ea49ea96f98c26f74788
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-27 19:37:37 +0400

    Medium:  Summary Ranges

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int:
-        if not len(nums):
-            return 0
+    def summaryRanges(self, nums: list[int]) -> list[str]:
+        result = []
+        i = 0
 
-        cache = set(nums)
-        result = 1
-        temp = 1
-        curr = cache.pop()
-
-        while cache:
-            if curr - 1 in cache:
-                cache.add(curr)
-                curr -= 1
-
-            elif curr + 1 in cache:
-                temp += 1
-                result = max(result, temp)
-                if curr in cache:
-                    cache.remove(curr)
-                curr += 1
-
+        while i < len(nums):
+            left = right = i
+            while right + 1 < len(nums) and nums[right] + 1 == nums[right + 1]:
+                right += 1
+            if left == right:
+                result.append(f'{nums[left]}')
             else:
-                temp = 1
-                curr = cache.pop()
+                result.append(f'{nums[left]}->{nums[right]}')
+            i = right + 1
 
         return result
 
 
+
 if __name__ == '__main__':
     task = Solution()
-    print(task.longestConsecutive(nums=[0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
+    print(task.summaryRanges(nums=[0, 1, 2, 4, 5, 7]))
 
     nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
     print(sorted(nums))

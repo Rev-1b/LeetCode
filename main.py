d79da28f9b1028b1f0f895b2d9b921f8784ece77
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-01-05 11:29:47 +0400

    Medium: Minimum Number of Arrows to Burst Balloons

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 class Solution:
-    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-        result = []
-        START, END = 0, 1
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+        points.sort(key=lambda x: x[1])
+        arrows = 1
+        shot = points[0][1]
 
+        for balloon in points[1:]:
+            if shot < balloon[0]:
+                arrows += 1
+                shot = balloon[1]
 
-        for elem in intervals:
-            if elem[END] < newInterval[START]:
-                result.append(elem)
-            elif elem[START] > newInterval[END]:
-                result.append(newInterval)
-                newInterval = elem
-            elif elem[START] <= newInterval[START] or elem[END] >= newInterval[END]:
-                newInterval = [min(elem[START], newInterval[START]),
-                               max(elem[END], newInterval[END])]
-        result.append(newInterval)
-        return result
+        return arrows
 
 
 if __name__ == '__main__':
     task = Solution()
-    print(task.insert(intervals=[[1, 3], [6, 9]], newInterval=[2, 5]))
+    print(task.findMinArrowShots(points=[[10, 16], [2, 8], [1, 6], [7, 12]]))

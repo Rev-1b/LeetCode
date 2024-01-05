d80389c4e0d497cb3067af0206703d51cef2e8f4
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-01-05 12:34:24 +0400

    Medium: Add Two Numbers

--- main.py
+++ main.py
@@ -8,20 +8,33 @@ class ListNode:
 
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return False
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = None
+        tail = None
+        prev_val = 0
 
-        slow, fast = head, head.next
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            temp_sum = val1 + val2 + prev_val
+            new_node_val = temp_sum % 10
+            prev_val = temp_sum // 10
 
-        while fast and fast.next:
-            if slow == fast:
-                return True
+            new_node = ListNode(new_node_val)
+            if not head:
+                head = new_node
+                tail = head
+            else:
+                tail.next = new_node
+                tail = new_node
 
-            slow = slow.next
-            fast = fast.next.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-        return False
+        if prev_val:
+            tail.next = ListNode(prev_val)
+
+        return head
 
 
 if __name__ == '__main__':
@@ -40,4 +53,4 @@ if __name__ == '__main__':
 
     node4.next = node9
 
-    print(task.hasCycle(node10))
+    print(task.addTwoNumbers(node10))

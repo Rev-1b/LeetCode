9db9426ac0fa527eb1840e222a6e32cb10b90ae6
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-01-06 21:14:29 +0400

    Medium: Reverse Linked List II

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 from typing import Optional
 
 
-class Node:
+class ListNode:
     def __init__(self, x, next=None):
         self.val = x
         self.next = next
-        self.random = None
 
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
 
-        old_head = head
-        new_head = Node(head.val)
-        new_tail = new_head
-        old_to_new = {old_head: new_head}
-        old_head = old_head.next
+        curr = head
+        tail = None
+        while left > 1:
+            tail = curr
+            curr = curr.next
+            left -= 1
+            right -= 1
 
-        while old_head:
-            new_node = Node(old_head.val)
-            old_to_new[old_head] = new_node
-            new_tail.next = new_node
-            new_tail = new_node
-            old_head = old_head.next
+        first_to_last = curr
+        prev = None
+        while right > 0:
+            next_node = curr.next
+            if prev:
+                curr.next = prev
+            prev = curr
+            curr = next_node
+            right -= 1
 
-        while head:
-            old_to_new.get(head).random = old_to_new.get(head.random)
-            head = head.next
-
-        return new_head
-
-
+        if tail:
+            tail.next = prev
+        else:
+            head = prev
+        first_to_last.next = curr
+        return head
 
 if __name__ == '__main__':
     task = Solution()
